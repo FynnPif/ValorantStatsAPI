dc8133d77a7c613cdd6c921d4ae11048d3614b3a
ErixXxYe <derstylervonmorgen@gmail.com>
2022-04-20 11:04:51 +0200

    nig

---
+++ src/Comps/test.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            name:'',
+            tag:'',
+        }
+    }
+
+    
+    valorantAPI = require('unofficial-valorant-api');
+    fetchAccount(name, tag) {
+        const acc = this.props.valorantAPI.getAccount(this.state.name, this.state.tag)
+        return(acc)
+    }
+    render(){
+        return(
+            <div>
+                <h1>{console.log(this.fetchAccount("Delflinger", "ZRK"))}</h1>
+            </div>
+        )
+    }
+
+ 
+
+    
+    //Do something with the data, for an example send it as a Discord Embed into your Discord
+}
+
+
+
+export default Test
