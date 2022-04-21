b2234e287837b795f3df2327efc439a18c5d7976
Vincent Jones <vinc.alex.jones@gmail.com>
2022-04-21 10:56:50 +0200

    stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Background from './Comps/Background';
 
 function App() {
   return (
     <div className="App">
-     
+     <Background/>
     </div>
   );
 }
---
+++ src/Comps/Background.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import styles from './Background.module.css'
+
+export default class Background extends Component {
+  render(){
+    return(
+      <div className={styles.background}>
+        <img id={styles.image} src='./Background.jpg'></img>
+      </div>
+    )
+  }
+
+}
