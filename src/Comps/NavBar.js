fc08a2bc9e1f75a5f42b0e19a8adc147160af112
ErixXxYe <derstylervonmorgen@gmail.com>
2022-05-04 11:00:05 +0200

    NavBar angefangen

---
+++ src/Comps/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+class NavBar extends Component {
+    state = {  } 
+    render() { 
+        return (
+        <nav >
+        <div>
+            NAV
+        </div>
+        </nav>);
+    }
+}
+ 
+export default NavBar;
